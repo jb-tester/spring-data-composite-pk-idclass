bbed5108d2dbac855ff7b7c60a7bd4e9e6fc2f76
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2025-03-24 17:22:28 +0100

    links

--- src/main/java/com/mytests/spring/springdatacompositepkidclass/repositories/EmployeeRepository.java
+++ src/main/java/com/mytests/spring/springdatacompositepkidclass/repositories/EmployeeRepository.java
@@ -14,7 +14,9 @@ public interface EmployeeRepository extends Repository<Employee, EmployeeId>{
     @Query("SELECT e FROM Employee e")
     List<Employee> findAllEmployees();
 
+    // https://youtrack.jetbrains.com/issue/IDEA-369016/Spring-JPA-findById-repository-method-is-shown-as-error-in-case-of-IdClass
     Optional<Employee> findById(EmployeeId id);
 
+    // https://youtrack.jetbrains.com/issue/IDEA-369676/Spring-JPA-false-error-is-shown-for-findAllByIdIterableID-method-parameter-in-case-of-IdClass
     Iterable<Employee> findAllById(Iterable<EmployeeId> ids);
 }
